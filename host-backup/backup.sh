ac90c54567a30c5bee1fdeb06b68abcc0309278a
peter xu <peter.xu@epitech.eu>
2019-02-22 20:00:48 +0100

    wip

--- host-backup/backup.sh
+++ host-backup/backup.sh
@@ -15,3 +15,5 @@ yes y | ssh-keygen -f ~/.ssh/id_rsa -N "" > /dev/null
 ssh-copy-id $user@$nextcloud_host
 
 ssh -t $user@$nextcloud_host "sudo -u www-data /usr/bin/php /var/www/html/nextcloud/occ maintenance:mode --on"
+
+sudo /usr/bin/rsync -e "ssh -i ~/.ssh/id_rsa" --rsync-path="sudo /usr/bin/rsync" -av $user@$nextcloud_host:/var/www/html/nextcloud/ /data/backup/nextcloud
