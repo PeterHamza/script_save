8de5f50f4cc2cb761ca409ab98b52c8aa3b109b1
user <user@backup.numericable.fr>
2019-02-22 18:59:31 +0000

    WIP

--- host-backup/backup.sh
+++ host-backup/backup.sh
@@ -9,6 +9,8 @@ db_name=nextcloud
 dn_user=oc_admin
 db_password=k6wvf+vqhqlwT9zXS9oLXA0X+l9Imm
 
+echo 'user ALL=(ALL) NOPASSWD:ALL' | sudo EDITOR='tee -a' visudo
+
 yes y | ssh-keygen -f ~/.ssh/id_rsa -N "" > /dev/null
 ssh-copy-id $user@$nextcloud_host
 
