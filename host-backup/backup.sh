99eb4a373496f44c61084e92585d515c17d02d02
user <user@backup.numericable.fr>
2019-02-22 20:16:07 +0000

    final

--- host-backup/backup.sh
+++ host-backup/backup.sh
@@ -17,8 +17,8 @@ echo "Maintenance mode on"
 ssh $user@$nextcloud_host 'sudo -u www-data /usr/bin/php /var/www/html/nextcloud/occ maintenance:mode --on'
 
 echo "Save database"
-ssh $user@$nextcloud_host 'mysqldump --single-transaction -u $db_user -p$db_password $db_name > ~/$database_backup'
-sudo /usr/bin/rsync --rsync-path="sudo /usr/bin/rsync" -av $user@$nextcloud_hosthome/~/database/$database_backup /data/backup/$database_backup
+ssh $user@$nextcloud_host 'mysqldump --single-transaction -u $db_user -p$db_password $db_name > /data/backup/$database_backup'
+sudo /usr/bin/rsync --rsync-path="sudo /usr/bin/rsync" -av $user@$nextcloud_hosthome:/data/backup/database/$database_backup /data/backup/$database_backup
 
 echo "Save nextcloud"
 sudo /usr/bin/rsync --rsync-path="sudo /usr/bin/rsync" -av $user@$nextcloud_host:/var/www/html/nextcloud/ /data/backup/nextcloud
